8de0f63e57a85840d9d547c4a39cff54a7915b06
Ardhi Lukianto <ardhi@lukianto.com>
2024-10-01 20:54:04 +0700

    Updates

--- bajo/hook/waibu-mpa.theme@after-inject-scripts.js
+++ bajo/hook/waibu-mpa.theme@after-inject-scripts.js
@@ -1,6 +1,6 @@
 async function waibuMpaThemefterInjectScripts ({ items, req }) {
   const { readJson } = this.app.bajo
-  const { pick, find, filter, isArray, map } = this.app.bajo.lib._
+  const { filter, isArray, map } = this.app.bajo.lib._
   items.push(`${this.name}.virtual:/alpinejs/cdn.min.js`)
   const all = filter(readJson(`${this.dir.pkg}/lib/libs.json`), f => this.config.plugins.includes(f.prefix))
   for (const a of all) {
@@ -8,10 +8,12 @@ async function waibuMpaThemefterInjectScripts ({ items, req }) {
     items.push(...map(scripts, s => `${this.name}.virtual:/${a.prefix}/${s}`))
   }
   // custom scripts
+  /*
   const theme = pick(find(this.app.waibuMpa.themes, { name: req.theme }) ?? {}, ['name', 'framework'])
   if (theme.name === 'bootstrap' || theme.framework === 'bootstrap') {
     items.push('waibuAlpinejs.asset:/js/bootstrap.js')
   }
+  */
 }
 
 export default waibuMpaThemefterInjectScripts
