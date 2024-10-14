890db1168c586f001e080f728fd06c6a45f6500c
Ardhi Lukianto <ardhi@lukianto.com>
2024-10-14 14:30:52 +0700

    Updates

--- bajo/hook/waibu-mpa.theme@after-inject-scripts.js
+++ bajo/hook/waibu-mpa.theme@after-inject-scripts.js
@@ -1,12 +1,12 @@
 async function waibuMpaThemefterInjectScripts ({ items, req }) {
   const { readJson } = this.app.bajo
-  const { filter, isArray, map } = this.app.bajo.lib._
-  items.push(`${this.name}.virtual:/alpinejs/cdn.min.js`)
+  const { filter, isString, map } = this.app.bajo.lib._
   const all = filter(readJson(`${this.dir.pkg}/lib/libs.json`), f => this.config.plugins.includes(f.prefix))
   for (const a of all) {
-    const scripts = isArray(a.scripts) ? [a.scipts] : a.scipts
-    items.push(...map(scripts, s => `${this.name}.virtual:/${a.prefix}/${s}`))
+    const scripts = isString(a.scripts) ? [a.scripts] : a.scripts
+    items.push(...map(scripts, s => `${this.name}.virtual:/${a.prefix}${s}`))
   }
+  items.push(`${this.name}.virtual:/alpinejs/cdn.min.js`)
   // custom scripts
   /*
   const theme = pick(find(this.app.waibuMpa.themes, { name: req.theme }) ?? {}, ['name', 'framework'])
--- waibuStatic/virtual.js
+++ waibuStatic/virtual.js
@@ -1,8 +1,8 @@
 async function virtual () {
   const { readJson } = this.app.bajo
-  const { pick, find } = this.app.bajo.lib._
+  const { pick } = this.app.bajo.lib._
+  const virts = [{ prefix: 'alpinejs', root: 'alpinejs:/dist' }]
   const libs = readJson(`${this.dir.pkg}/lib/libs.json`)
-  const virts = [find(libs, { prefix: 'alpinejs' })]
   for (const l of libs) {
     if (this.config.plugins.includes(l.prefix)) virts.push(pick(l, ['prefix', 'root']))
   }
